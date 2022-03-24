bbb633004508bf6d5f9f272ae1cebd4619091a66
窦道虎 <fm00940@office.freemud.cn>
2022-03-24 12:13:40 +0800

    init

---
+++ test/env.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"github.com/ddh-open/gin/framework"
+	"github.com/ddh-open/gin/framework/provider/app"
+	"github.com/ddh-open/gin/framework/provider/config"
+	"github.com/ddh-open/gin/framework/provider/env"
+)
+
+func InitBaseContainer() *framework.NiceContainer {
+	// 初始化服务容器
+	container := framework.NewNiceContainer()
+	// 绑定App服务提供者
+	container.Bind(&app.NiceAppProvider{})
+	// 后续初始化需要绑定的服务提供者...
+	container.Bind(&env.NiceEnvProvider{})
+	container.Bind(&config.NiceConfigProvider{})
+	return container
+}
